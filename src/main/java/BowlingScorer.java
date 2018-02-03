600df4c39c95ca228f93aeb41e0476e1130456d2
richaagrawal <richaagr@thoughtworks.com>
2018-02-03 15:03:11 +1100

    Adding sum and spare scores

--- src/main/java/BowlingScorer.java
+++ src/main/java/BowlingScorer.java
@@ -1,2 +1,46 @@
-public class BowlingScorer {
+import java.util.ArrayList;
+import java.util.List;
+
+public class BowlingScorer implements BowlingGame {
+
+    private static final int WINNING_FRAME_SCORE = 10;
+    private static final int TOTAL_FRAMES = 10;
+    private List<Integer> rolledPins;
+
+    BowlingScorer() {
+        this.rolledPins = new ArrayList<>();
+    }
+
+    @Override
+    public void roll(int pins) {
+        rolledPins.add(pins);
+    }
+
+    @Override
+    public int score() {
+        int score = 0;
+        int rollsInFrame = 0;
+        for (int frame = 0; frame < TOTAL_FRAMES; frame++) {
+            if (isSpare(rollsInFrame)) {
+                score += WINNING_FRAME_SCORE + spareScore(rollsInFrame);
+                rollsInFrame += 2;
+            } else {
+                score += rollSumInFrame(rollsInFrame);
+                rollsInFrame += 2;
+            }
+        }
+        return score;
+    }
+
+    private boolean isSpare(int frame) {
+        return rollSumInFrame(frame) == WINNING_FRAME_SCORE;
+    }
+
+    private int rollSumInFrame(int rollInFrame) {
+        return rolledPins.get(rollInFrame) + rolledPins.get(rollInFrame + 1);
+    }
+
+    private int spareScore(int spareFrameRoll) {
+        return rolledPins.get(spareFrameRoll + 2);
+    }
 }
--- src/test/java/BowlingScorerTest.java
+++ src/test/java/BowlingScorerTest.java
@@ -1,3 +1,40 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class BowlingScorerTest {
 
+    private BowlingScorer bowlingScorer;
+
+    @Before
+    public void setUp() {
+        bowlingScorer = new BowlingScorer();
+    }
+
+    @Test
+    public void shouldAddNumberOfPins() {
+        bowlingScorer.roll(1);
+        bowlingScorer.roll(6);
+        rollMultipleWithNoPins(18);
+
+        assertEquals(7, bowlingScorer.score()); //(1+6)
+    }
+
+    @Test
+    public void shouldAddSpareScoreIfSpare() {
+        bowlingScorer.roll(4);
+        bowlingScorer.roll(6);
+        bowlingScorer.roll(4);
+        bowlingScorer.roll(5);
+        rollMultipleWithNoPins(16);
+
+        assertEquals(23, bowlingScorer.score()); //(4+6+4)+(4+5)
+    }
+
+    private void rollMultipleWithNoPins(int rollWithNoPins) {
+        for (int i = 0; i < rollWithNoPins; i++) {
+            bowlingScorer.roll(0);
+        }
+    }
 }
